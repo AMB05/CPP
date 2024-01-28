1580db96643ccfd019962e36f3f87b27ae5ba2f6
Arriva <92034212+AMB05@users.noreply.github.com>
2024-01-28 20:46:53 +0700

    Percabangan dan Perulangan

---
+++ Pertemuan 5 (Kombinasi)/src/main.cpp
@@ -0,0 +1,197 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+
+using namespace std;
+
+int main () //fungsi utama
+{
+    
+}
+
+    // int  ulang, Pilih;
+    // float jumlah, kurang, bagi, kali, bil1, bil2;
+
+    // cout << "Program Kalkulator" << endl;
+    // cout << "1. Jumlah" << endl;
+    // cout << "2. Kurang" << endl;
+    // cout << "3. Kali "  << endl;
+    // cout << "4. Bagi "  << endl;
+
+    // cout << "Berapa banyak anda ingin menjalankan Program ?" << endl;
+    // cin  >> ulang;
+
+    // for(int i=0; i<ulang; i++)
+    // {
+    //     cout << "Input Pilihan Kalkulator" << endl;
+    //     cin  >> Pilih;
+
+    //     if(Pilih == 1)
+    //     {
+    //         cout << "Input Bilangan Pertama " << endl;
+    //         cin  >> bil1;
+    //         cout << "Input Bilangan Kedua " << endl;
+    //         cin  >> bil2;
+    //         jumlah = bil1 + bil2; //proses perhitungan
+
+    //         cout << "Hasil  = " << jumlah << endl;
+
+    //     }
+    // }
+
+
+    // int pilih, ulang, barang;
+    // int harga1, harga2, total;
+    // string nama, member, jenis_member;
+    // string nama_barang1, diskon;
+
+    // cout << "Program Kasir" << endl;
+    // cout << "------------" << endl;
+    // cout << "Input Nama" << endl;
+    // cin  >> nama;
+    // cout << "Berapa banyak anda belanja barang ?" << endl;
+    // cin  >> barang;
+
+    // for(int i=0; i<barang; i++)
+    // {
+    //     cout << "Input Nama Barang ";
+    //     cin  >> nama_barang1;
+    //     cout << "Input Harga Barang ";
+    //     cin  >> harga1;
+
+    //     cout << "Apakah Anda Punya Member ? ";
+    //     cin  >> member;
+
+    //     if (member == "ya" || "YA" || "Ya")
+    //     {
+    //         cout << "Apakah Jenis Member Anda ? ";
+    //         cin  >> jenis_member;
+
+    //         if (jenis_member == "GOLD" || "Gold" || "gold")
+    //         {
+    //             diskon =  "Anda mendapatkan Potongan 15%";
+    //         }
+    //         else if (jenis_member == "Silver" || "SILVER" || "silver")
+    //         {
+    //             diskon =  "Anda mendapatkan Potongan 10%";
+    //         }
+    //         else if (jenis_member == "bronze" || "BRONZE" || "bronze")
+    //         {
+    //             diskon =  "Anda mendapatkan Potongan 5%";
+    //         }
+    //     }
+    //     else if (member == "tidak" || "TIDAK" || "Tidak")
+    //     {
+    //         diskon =  "Anda Tidak Dapat Diskon heheheh :D";
+    //     }
+    //     else
+    //     {
+    //         cout <<" Salah Input ";
+    //     }
+
+
+    //     cout << " Anda Belanja Barang " << nama_barang1 << " dengan harga "
+    //          << harga1 << " Anda mememiliki member dan " << diskon;
+
+    //     cout << endl;
+
+    // }
+
+
+
+// int pilih, ulang;
+//     float r, p, l, s, a, t, luas;
+
+//     cout << "Program Menghitung Luas Bangun Datar " << endl;
+//     cout << "-------------------------------------" << endl;
+//     cout << "1. Luas Lingkaran " << endl;
+//     cout << "2. Luas Persegi " << endl;
+//     cout << "3. Luas Segitiga " << endl;
+//     cout << "4. Luas Persegi Panjang " << endl;
+//     cout << "5. Exit " << endl; 
+//     cout << "--------------------------------------" << endl;
+
+//     cout << "Berapa Kali Anda Ingin Mengulang Program ? " << endl;
+//     cin  >> ulang;
+
+//     int i =0;
+//     while (i<ulang)
+//     {
+//         cout << endl;
+//         cout << "----------------------------" << endl;
+//         cout << "Input Pilihan (1/2/3/4/5) : "; 
+//         cin  >> pilih;
+
+//         switch (pilih)
+//         {
+//         case 1:
+//             cout << "-------------------------" << endl;
+//             cout << "Menghitung Luas Lingkaran" << endl;
+//             cout << "Input Jari-Jari = "; 
+//             cin  >> r;
+//             luas = 3.14*r*r;
+//             cout << "Luas Lingkaran = " << luas << " Cm^2" << endl;
+//             break;
+
+//         case 2:
+//             cout << "-----------------------" << endl;
+//             cout << "Menghitung Luas Persegi" << endl;
+//             cout << "Input Sisi = "; 
+//             cin  >> s;
+//             luas = s*s;
+//             cout << "Luas Persegi = " << luas << " Cm^2" << endl;
+//             break;
+
+//         case 3:
+//             cout << "------------------------" << endl;
+//             cout << "Menghitung Luas Segitiga" << endl;
+//             cout << "Input Alas = "; 
+//             cin  >> a;
+//             cout << "Input Tinggi = "; 
+//             cin  >> t;
+//             luas = 0.5*a*t;
+//             cout << "Luas Segitiga = " << luas << " Cm^2" << endl;
+//             break;
+        
+//         case 4:
+//             cout << "-------------------------------" << endl;
+//             cout << "Menghitung Luas Persegi Panjang" << endl;
+//             cout << "Input Panjang = "; 
+//             cin  >> p ;
+//             cout << "Input Lebar = ";
+//             cin  >> l;
+//             cout << "Input Tinggi = "; 
+//             cin  >> t;
+//             luas = p*l*t;
+//             cout << "Luas Persegi Panjang = " << luas << " Cm^2" << endl;
+        
+//         case 5:
+//             exit(5);
+        
+//         default: cout << "Salah Input !!!"<< endl;
+//             break;
+//         }
+
+//         i++;
+//     }
+
+
+
+
+
+    
+
+/////////////////////////////////////////////////////////
+
+    //     int i = 0;
+	// while(i <= 10)
+    // {
+	// 	i++;
+	// 	if(i == 5)
+    //     {
+	// 		continue;
+	// 		//break;
+	// 	}
+	// 	cout << i << endl;
+	// }
+    // cout << "akhirnya" << endl; 
