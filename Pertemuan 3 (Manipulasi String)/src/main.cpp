06156b016c0a4ae0f2515fbf79e198841693cb6b
Arriva <92034212+AMB05@users.noreply.github.com>
2024-01-28 20:45:14 +0700

    Manipulasi String
    Manipulasi String

---
+++ Pertemuan 3 (Manipulasi String)/src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+using namespace std;
+
+int main ()  //fungsi
+{
+    cout << endl;
+
+    char bunga[15] = "Mawar";
+
+    cout<<"Panjang Karakter Kata " << bunga <<
+            " adalah " << strlen(bunga) <<endl;
+
+}
+
+    // char teks[13];
+    
+    // cout<<"Input Kata " << endl;
+    // cin >>teks;
+    // cout<<"Kata Tercetak " << teks <<endl;
+    // getch(); 
